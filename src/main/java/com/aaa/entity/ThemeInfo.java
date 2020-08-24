2b3d9f885279e8e2b02d5d1e31b1890f2c463377
rendi <273100622@qq.com>
2020-08-24 19:46:12 +0800

    主题表的实体类

---
+++ src/main/java/com/aaa/entity/ThemeInfo.java
@@ -0,0 +1,40 @@
+package com.aaa.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/*
+    任帝：
+        themeinfo
+        主题表
+ */
+@Data
+public class ThemeInfo {
+//    编号
+    private Integer tid;
+
+//    主题名
+    private String tname;
+
+//    主题描述
+    private String tdesc;
+
+//    封面图
+    private String coverpic;
+
+//    主题类型
+    private Integer typeid;
+
+//    用户id
+    private Integer uid;
+
+//    主题状态
+    private Integer state;
+
+//    开始时间
+    private Date  starttime;
+
+//    结束时间
+    private Date endtime;
+}

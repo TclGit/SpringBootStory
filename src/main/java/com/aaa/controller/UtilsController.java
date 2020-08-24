37d2f71d51a5505e740ee85be50d1fa14459a713
lhm <2579159757@qq.com>
2020-08-24 17:26:38 +0800

    李慧敏

--- src/main/java/com/aaa/controller/UtilsController.java
+++ src/main/java/com/aaa/controller/UtilsController.java
@@ -1,10 +1,11 @@
 package com.aaa.controller;
 
 import com.aaa.service.PromissionService;
+import com.aaa.service.impl.UserServiceImpl;
 import com.aaa.until.JwtUtils;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.catalina.User;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +19,14 @@ public class UtilsController {
     @Resource
     PromissionService promissionService;
 
+    //李慧敏的专属类型
+    @Resource
+    private UserServiceImpl userServiceImpl;
+
+
+
+
+
     @RequestMapping("find")
     public Object find(HttpServletRequest request){
         String token = request.getHeader("token");
@@ -29,5 +38,57 @@ public class UtilsController {
 
         return promiss;
     }
+
+
+
+
+    /***
+     *    李慧敏的专属区
+     *    findAll
+     *    addUser
+     *    updateUser
+     *    deleteUser
+     * @return
+     */
+
+
+    @RequestMapping(value = "findAll",method = RequestMethod.POST)
+    public List<User> findAll(){
+        System.out.println("用户查询Controller");
+        return userServiceImpl.findAll();
+    }
+
+
+    @RequestMapping(value = "addUser",method = RequestMethod.POST)
+    public Integer addUser(@RequestBody User user){
+        return userServiceImpl.addUser(user);
+    }
+
+
+    @RequestMapping(value = "updateUser",method = RequestMethod.POST)
+    @ResponseBody
+    public Integer updateUser(@RequestBody User user){
+        return userServiceImpl.updateUser(user);
+    }
+
+
+    /**
+     * @PathVariable 映射 URL 绑定的占位符
+     * @param uid
+     * @return
+     */
+    @RequestMapping(value = "deleteUser/{uid}",method = RequestMethod.POST)
+    public Integer deleteUser(@RequestBody @PathVariable("uid") Integer uid){
+        return userServiceImpl.deleteUser(uid);
+    }
+
+
+    @RequestMapping(value = "updateState/{uid}/{state}",method = RequestMethod.POST)
+    public Integer update(@PathVariable("state") Integer state,@PathVariable("uid") Integer uid){
+        System.out.println("修改用户状态");
+        System.out.println(state+""+uid);
+        return userServiceImpl.updateState(state,uid);
+    }
+
 
 }

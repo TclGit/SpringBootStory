bf385fc4edc85e210037a9e25af7699f3d47274b
ml <2265625611@qq.com>
2020-08-26 08:56:33 +0800

    收支明细

---
+++ src/main/java/com/aaa/dao/BillInfoDao.java
@@ -0,0 +1,10 @@
+package com.aaa.dao;
+
+import com.aaa.entity.Billinfo;
+
+import java.util.List;
+
+public interface BillInfoDao {
+
+    List<Billinfo> listAll_Bbillinfo();
+}
---
+++ src/main/java/com/aaa/service/BillInfoService.java
@@ -0,0 +1,13 @@
+package com.aaa.service;
+
+import com.aaa.entity.Billinfo;
+
+import java.util.List;
+
+/**
+ * 马琳
+ */
+public interface BillInfoService {
+
+    List<Billinfo> listAll_BillInfo();
+}

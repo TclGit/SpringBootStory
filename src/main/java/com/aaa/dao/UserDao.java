6a94ae2167ae5a91ccfcd261977701eb72adecb1
ml <2265625611@qq.com>
2020-08-28 15:25:47 +0800

    a

--- src/main/java/com/aaa/dao/UserDao.java
+++ src/main/java/com/aaa/dao/UserDao.java
@@ -5,7 +5,6 @@ import org.apache.catalina.User;
 import java.util.List;
 
 public interface UserDao {
-
     /**
      * 后台
      * @return
@@ -13,10 +12,6 @@ public interface UserDao {
     List<User> findAll();
     //修改用户的状态
     Integer updateState(Integer uid,Integer state);
-
-
-
-
 
     Integer addUser(User user);
 
